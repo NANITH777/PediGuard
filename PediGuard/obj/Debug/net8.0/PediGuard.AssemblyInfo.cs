746c90b0fa93b0b9056c4372e704c24dbd6af0a8
NANITH777 <nanifulchany@gmail.com>
2024-11-15 21:27:01 +0300

    Home Page

--- PediGuard/obj/Debug/net8.0/PediGuard.AssemblyInfo.cs
+++ PediGuard/obj/Debug/net8.0/PediGuard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b3033cb55f9f105ffd517a9530de2feddfa12aa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f105f3b800ba7d4c2ef69eeddf733b228915f4b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
