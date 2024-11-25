0a3f5093095281e23e31ff8c9568463300573e5c
NANITH777 <nanifulchany@gmail.com>
2024-11-25 19:53:17 +0300

    Update Layout

---
+++ PediGuard/Models/Appointment.cs
@@ -0,0 +1,25 @@
+﻿using NuGet.DependencyResolver;
+
+namespace PediGuard.Models
+{
+    public class Appointment
+    {
+        public int Id { get; set; }
+
+        public int AssistantId { get; set; }
+        public Assistant Assistant { get; set; } 
+
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; } 
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public int Duration { get; set; }
+
+        public string Status { get; set; } 
+    }
+
+}
--- PediGuard/obj/Debug/net8.0/PediGuard.AssemblyInfo.cs
+++ PediGuard/obj/Debug/net8.0/PediGuard.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e0bfc4b01d33947eecdc436b0eb8b97e6a76d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86f3bce8b00325ed6eb97823dc53f9d8c5f97507")]
 [assembly: System.Reflection.AssemblyProductAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
