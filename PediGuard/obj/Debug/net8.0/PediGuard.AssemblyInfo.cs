6b566dfee1ee40fc5959bd6d771e7af26775e73c
NANITH777 <nanifulchany@gmail.com>
2024-11-27 19:47:17 +0300

    Emergency View

--- PediGuard/obj/Debug/net8.0/PediGuard.AssemblyInfo.cs
+++ PediGuard/obj/Debug/net8.0/PediGuard.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e2cb600ba24bc004e797257ff4ddb9d7b256a3a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8193ec1b58000267ebd141b4349a0e2f58e7006")]
 [assembly: System.Reflection.AssemblyProductAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PediGuard")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
